8d64214b4368c0f2db41b8e044bc2f2911757a1e
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-05 11:26:28 +0700

    memberi style pada main section

--- src/components/Main/MainSection.js
+++ src/components/Main/MainSection.js
@@ -1,11 +1,19 @@
 import GameCards from "./GameCards";
 import ScoreAndMatches from "./ScoreAndMatches";
+import background from "../../assets/images/bg.webp";
 
 const MainSection = (props) => {
   return (
-    <div>
-      <ScoreAndMatches />
-      <GameCards />
+    <div className="w-full h-[100vh] relative bg-red-400 flex">
+      <img
+        src={background}
+        alt="background nature"
+        className="m-auto scale-[350%]"
+      />
+      <div className="absolute top-0 right-0 left-0">
+        <ScoreAndMatches />
+        <GameCards />
+      </div>
     </div>
   );
 };
