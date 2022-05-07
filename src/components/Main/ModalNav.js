ccfe3326773685b881b125a6648173af4a37c036
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-07 14:19:58 +0700

    memperbaiki style modalnav & score&matches

--- src/components/Main/ModalNav.js
+++ src/components/Main/ModalNav.js
@@ -2,11 +2,11 @@ import ModalCard from "../UI/ModalCard";
 
 const ModalNav = (props) => {
   return (
-    <ModalCard className="px-3 py-5 flex flex-col justify-evenly bg-white z-50">
-      <button className="mb-3 py-2 w-full border-b-2 border-b-primaryGreen outline-none bg-transparent text-slate-700 hover:text-primaryGreen">
+    <ModalCard className="px-3 py-5 flex flex-col justify-evenly bg-primaryGreen z-50">
+      <button className="mb-3 py-2 w-full border-b-2 border-b-white outline-none bg-transparent text-white hover:text-slate-600">
         Reset
       </button>
-      <button className="py-2 w-full border-b-2 border-b-primaryGreen outline-none bg-transparent text-slate-700 hover:text-primaryGreen">
+      <button className="py-2 w-full border-b-2 border-b-white outline-none bg-transparent text-white hover:text-slate-600">
         Quit
       </button>
     </ModalCard>
--- src/components/Main/ScoreAndMatches.js
+++ src/components/Main/ScoreAndMatches.js
@@ -2,12 +2,12 @@ import MediaCard from "../UI/MediaCard";
 
 const ScoreAndMatches = (props) => {
   return (
-    <div className="w-full flex justify-evenly items-center pt-3 pb-5">
+    <div className="w-full flex justify-evenly items-center pt-3 pb-5 text-white">
       <MediaCard className="p-4 text-lg text-center">
         <h2 className="font-semibold">Mathces</h2>
         <p className="font-bold">2</p>
       </MediaCard>
-      <MediaCard className="py-4 px-7 text-lg text-center">
+      <MediaCard className="py-4 px-7 text-lg text-center text-white">
         <h2 className="font-semibold">Score</h2>
         <p className="font-bold">2</p>
       </MediaCard>
