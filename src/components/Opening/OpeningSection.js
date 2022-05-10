94631a3e566178f498adc3202e6d4908f0f60dcc
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-10 09:57:14 +0700

    memberi style pada bg-op & h1

--- src/components/Opening/OpeningSection.js
+++ src/components/Opening/OpeningSection.js
@@ -1,10 +1,16 @@
+import bgOp from "../../assets/images/bg-op.webp";
+
 const OpeningSection = (props) => {
   return (
-    <div className="relative w-full h-[100vh] overflow-hidden">
-      <img src="" alt="" />
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-red-400 flex flex-col items-center">
+    <div className="relative w-full h-[100vh] overflow-hidden flex">
+      <img
+        src={bgOp}
+        alt="background grass and trees"
+        className="m-auto w-full h-full brightness-75"
+      />
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center">
         <div className=" w-full h-[20%] flex">
-          <h1 className="text-3xl m-auto font-bold text-slate-800">
+          <h1 className="text-3xl m-auto font-bold text-red-700">
             Memory Game App
           </h1>
         </div>
@@ -24,8 +30,8 @@ const OpeningSection = (props) => {
             Your Highest Score: 50
           </p>
         </div>
-        <div className="flex  w-full h-[55%]">
-          <div className="w-3/4 h-3/4 m-auto grid grid-cols-2 grid-rows-2 bg-yellow-400">
+        <div className="flex  w-full h-[55%] ">
+          <div className="w-3/4 h-3/4 m-auto grid grid-cols-2 grid-rows-2 bg-[rgba(0,0,0,.3)] backdrop-blur-lg shadow-lg">
             <div className="bg-primaryGreen"></div>
             <div className="bg-orange-400"></div>
             <div className="bg-blue-400"></div>
